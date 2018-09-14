839bbda79e5ee1234c2c827b1f4d522b3ca7d779
Ahmet Yasir Tug <ahmetyasirtug@gmail.com>
2018-09-14 15:13:17 +0300

    kod güzelleştirildi

--- arduino-src/mainreadcanson/mainreadcanson.ino
+++ arduino-src/mainreadcanson/mainreadcanson.ino
@@ -31,7 +31,7 @@ union ArrayToInteger {
 
 union ArrayToDouble {
     byte array[4];
-    double doubl;
+    double number;
 } doubler;
 
 MS5803 prsensor(ADDRESS_HIGH); // basinc - sicaklik sensoru
@@ -133,19 +133,18 @@ void loop()
     {
         pressure_abs = prsensor.getPressure(ADC_4096);
         temperature_c = prsensor.getTemperature(CELSIUS, ADC_4096);
-        //
         
         Serial.println(pressure_abs);
         Serial.println(temperature_c);
-        doubler.doubl = pressure_abs;
-
+        
         canSnd.can_id = 0x03;
         canSnd.can_dlc = 8;
+        doubler.number = pressure_abs;
         canSnd.data[0] = doubler.array[0];
         canSnd.data[1] = doubler.array[1];
         canSnd.data[2] = doubler.array[2];
         canSnd.data[3] = doubler.array[3];
-        doubler.doubl = temperature_c;
+        doubler.number = temperature_c;
         canSnd.data[4] = doubler.array[0];
         canSnd.data[5] = doubler.array[1];
         canSnd.data[6] = doubler.array[2];
