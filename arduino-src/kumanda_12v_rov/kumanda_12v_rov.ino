bc282e3722196b9da756eb036c2818ac334c009f
Ahmet Yasir Tug <ahmetyasirtug@gmail.com>
2018-09-14 16:25:57 +0300

    eski kodlar silindi, kod duzenlendi, kullanilmayan degisken silindi

--- arduino-src/kumanda_12v_rov/kumanda_12v_rov.ino
+++ arduino-src/kumanda_12v_rov/kumanda_12v_rov.ino
@@ -2,6 +2,8 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
+
+
 struct can_frame canMsg1;
 struct can_frame canMsg2;
 
@@ -10,9 +12,10 @@ MCP2515 mcp2515(10);
 
 float hizBoleni = 1.0;
 
-int maxdeger = 2000; //max 2000 oluyor escler 1000-2000 arası calisir
-int mindeger = 3000 - maxdeger;
-int pressure;
+#define maxdeger 2000 //max 2000 oluyor escler 1000-2000 arası calisir
+#define mindeger 1000
+#define sabitleme_toleransi 30
+
 int pinJoyStick_X_1 = 2;
 int pinJoyStick_Y_1 = 3;
 int pinJoyStick_X_2 = 1;
@@ -40,17 +43,6 @@ void setup()
     mcp2515.setBitrate(CAN_125KBPS);
     mcp2515.setNormalMode();
 
-    /*canMsg2.can_id = 0x03;
-    canMsg2.can_dlc = 8;
-    canMsg2.data[0] = 0x01;
-    canMsg2.data[1] = 0x01;
-    canMsg2.data[2] = 0x01;
-    canMsg2.data[3] = 0x00;
-    canMsg2.data[4] = 0x00;
-    canMsg2.data[5] = 0x00;
-    canMsg2.data[6] = 0x00;
-    canMsg2.data[7] = 0xA0;*/
-
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
     pinMode(8, INPUT);
@@ -58,7 +50,6 @@ void setup()
     pinMode(6, INPUT);
     pinMode(9, INPUT);
 
-    //while (!Serial);
 }
 
 void loop()
@@ -102,6 +93,7 @@ void loop()
     valueJoyStick_X_2 = 1500 + (valueJoyStick_X_2 - 1500) / hizBoleni;
     valueJoyStick_Y_2 = 1500 + (valueJoyStick_Y_2 - 1500) / hizBoleni;
     valueJoyStick_X_1 = 3000 - valueJoyStick_X_1;
+    
     if (valueJoyStick_X_1 > maxdeger)
         valueJoyStick_X_1 = maxdeger;
 
@@ -120,16 +112,16 @@ void loop()
     if (valueJoyStick_X_2 < mindeger)
         valueJoyStick_X_2 = mindeger;
 
-    if (valueJoyStick_X_1 < 1530 && valueJoyStick_X_1 > 1470)
+    if (valueJoyStick_X_1 < 1500 + sabitleme_toleransi && valueJoyStick_X_1 > 1500 - sabitleme_toleransi)
         valueJoyStick_X_1 = 1500;
 
-    if (valueJoyStick_Y_1 < 1530 && valueJoyStick_Y_1 > 1470)
+    if (valueJoyStick_Y_1 < 1500 + sabitleme_toleransi && valueJoyStick_Y_1 > 1500 - sabitleme_toleransi)
         valueJoyStick_Y_1 = 1500;
 
-    if (valueJoyStick_X_2 < 1530 && valueJoyStick_X_2 > 1470)
+    if (valueJoyStick_X_2 < 1500 + sabitleme_toleransi && valueJoyStick_X_2 > 1500 - sabitleme_toleransi)
         valueJoyStick_X_2 = 1500;
 
-    if (valueJoyStick_Y_2 < 1530 && valueJoyStick_Y_2 > 1470)
+    if (valueJoyStick_Y_2 < 1500 + sabitleme_toleransi && valueJoyStick_Y_2 > 1500 - sabitleme_toleransi)
         valueJoyStick_Y_2 = 1500;
 
     canMsg1.can_id = 0x02;
@@ -143,17 +135,6 @@ void loop()
     canMsg1.data[6] = highByte(valueJoyStick_Y_2);
     canMsg1.data[7] = lowByte(valueJoyStick_Y_2);
 
-    /* canMsg2.can_id = 0x03;
-    canMsg2.can_dlc = 8;
-    canMsg2.data[0] = bag1;//bag1
-  canMsg2.data[1] = bag2;//bag2
-  canMsg2.data[2] = kol;
-  /*canMsg2.data[3] = 0x01;
-  canMsg2.data[4] = 0x01;
-  canMsg2.data[5] = 0x01;
-  canMsg2.data[6] = 0x01;
-  canMsg2.data[7] = 0xA0;*/
-
     Serial.print('q');
     Serial.println(valueJoyStick_X_1);
     Serial.print('w');
@@ -166,10 +147,7 @@ void loop()
     if (valueJoyStick_Y_1 < 1550 && valueJoyStick_Y_1 > 1450)
         valueJoyStick_Y_1 = 1500;
 
-    // if(Serial.available())
-    //value = Serial.parseInt();    // Parse an Integer from Serial
 
     mcp2515.sendMessage(&canMsg1);
-    //mcp2515.sendMessage(&canMsg2);
     delay(100);
 }
